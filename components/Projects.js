b38f8a00a767c118d375ebf1298a9f54204d8e59
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-03 13:46:52 +0930

    now render all projects from json file information

--- components/Projects.js
+++ components/Projects.js
@@ -1,25 +1,25 @@
 import { Grid, Heading, Box, Text } from '@chakra-ui/react';
 import ProjectCard from './ui/ProjectCard';
 
+// temp holding place for project data -> currently figuring out best solution for this
+let projects = require('../data/projects.json');
+let hero = projects[0];
+let otherProjects = projects.slice(1);
+
 const Projects = props => {
   return (
     <section>
-      <Grid gap='1rem' templateRows='1fr' templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)','repeat(3, 1fr)','repeat(4, 1fr)']} mt='2rem'>
-        <Box gridRowStart={[2, 2, 1]} gridRowEnd={[2, 2, 3]} gridColumn={1}>
-          <ProjectCard image='/images/tech-blog-mobile.png' title='Tech Blog' />
-        </Box>
-        <ProjectCard image='/images/agency-landing-page.png' title='Agency Landing Page' gridColumnStart={1} gridColumnEnd={2} />
-        <ProjectCard image='/images/calculator.jpg' title='JS Calculator' />
-        <ProjectCard image='/images/trip_book.png' title='trip book' />
-        <ProjectCard image='/images/weather-dashboard.png' title='Weather Dashboard' />
-        <ProjectCard image='/images/employee-tracker.png' title='Employee SQL Node Interface' />
-        <ProjectCard image='/images/javascript-quiz.png' title='Javascript Quiz' />
+      <Grid gap='1rem' templateRows='1fr' templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)','repeat(3, 1fr)','repeat(3, 1fr)','repeat(4, 1fr)']} mt='2rem'>
         <Box gridColumn={[1, 1, 2]} gridRowStart={1} gridRowEnd={2} alignSelf='center' justifySelf={'center'}>
-          <Heading as='h2' fontSize={['4rem','5rem', '6rem', '7rem', '8rem']} display={'flex'} flexDirection={['row', 'row','row', 'column']} lineHeight={'6rem'}>
+          <Heading as='h2' fontSize={['4rem','5rem', '6rem', '7rem', '9rem']} display={'flex'} flexDirection={['row', 'row','row', 'column']} lineHeight={['6rem', '7rem']}>
             <Text as={'span'}>Pro</Text>
             <Text as={'span'}>jects</Text>
           </Heading>
         </Box>
+        <Box gridRowStart={[2, 2, 1]} gridRowEnd={[2, 2, 3]} gridColumn={1}>
+          <ProjectCard image={hero.image} title={hero.title} website={hero.website} github={hero.github} />
+        </Box>
+        {otherProjects.map(project => <ProjectCard image={project.image} title={project.title} website={project.website} github={project.github} />)} 
       </Grid>
     </section>
   );
